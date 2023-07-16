9ae8912a33c5a95a8bd0a4e73d65b36c81372774
Luiz Ferreira <luiz.ferreira@outlook.com>
2023-07-17 01:09:20 +0200

    fix: download url path

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for cloudcustodian
-	url="$GH_REPO/archive/refs/tags/${version}.tar.gz"
+	url="$GH_REPO/archive/${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -60,14 +60,15 @@ install_version() {
 	echo "* Installing $TOOL_NAME release $version..."
 
 	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-		# TODO: Assert cloudcustodian executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
+		# mkdir -p "$install_path"
+		# cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		# # TODO: Assert cloudcustodian executable exists.
+		# local tool_cmd
+		# tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		# test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+		# echo "$TOOL_NAME $version installation was successful!"
 	) || (
 		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
